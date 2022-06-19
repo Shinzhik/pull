473977eb97c60d0a185c13676ab5a958d1e98769
KingG1Love <92129589+KingG1Love@users.noreply.github.com>
2022-06-19 18:12:12 +1000

    2 Chast

--- CODE.py
+++ CODE.py
@@ -1,7 +1,23 @@
+    F = (p - 1) * (q - 1)
+    for i in range(2, F):
+        if (gcd(i, F) == 1):
+            e = i
+            break
+i = 0
+    while (i * e % F != 1):
+        i += 1
+        d = i
+        if i == e:
+            i += 1
+    C = M ** e % n
+    print("crypt message: ",C)
+    print(e)
+    print(d)
+    print(n)
 elif Vopros == 1:
     print("Enter Private Key")
     d = int(input("d = "))
     n = int(input("n = "))
-    c = int(input("Enter what need to decrypt  = "))  # i*e % F = 1 !@! i*d = 1 (mod F)
+    c = int(input("Enter what need to decrypt  = "))  # ie % F = 1 !@! id = 1 (mod F)
     M = (c ** d) % n
     print("Decrypt message = ",M)
