1b7853291ae18b0204798d0ced9fd780d458f571
Shinzhik <92129911+Shinzhik@users.noreply.github.com>
2022-06-19 18:06:08 +1000

    Delete CODE.py

--- CODE.py
+++
@@ -1,7 +0,0 @@
-elif Vopros == 1:
-    print("Enter Private Key")
-    d = int(input("d = "))
-    n = int(input("n = "))
-    c = int(input("Enter what need to decrypt  = "))  # i*e % F = 1 !@! i*d = 1 (mod F)
-    M = (c ** d) % n
-    print("Decrypt message = ",M)
